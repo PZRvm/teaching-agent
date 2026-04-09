d0cd07ed168128aab6c74450c643891e9d3a8c56
PZRvm <909045182@qq.com>
2026-04-09 11:33:39 +0800

    feat(observation-frontend): 添加观察模式路由配置

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './views/LandingPage'
+import ObservationConfig from './views/ObservationConfig'
+import ObservationView from './views/ObservationView'
 import NotFoundPage from './views/NotFoundPage'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/observation/config" element={<ObservationConfig />} />
+      <Route path="/observation/session/:sessionId" element={<ObservationView />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
